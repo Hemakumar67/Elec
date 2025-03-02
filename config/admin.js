6a9e7dcc003f06f11c14d3c9044ba3adc0079120
Hemakumar67 <rainahemmarshmi67@gmail.com>
2025-03-03 00:00:14 +0530

    leatest commit

--- config/admin.js
+++ config/admin.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', env('JWT_SECRET')),
   },
   apiToken: {
     salt: env('API_TOKEN_SALT', 'your-randomly-generated-salt'),
--- config/middlewares.js
+++ config/middlewares.js
@@ -1,5 +1,8 @@
 module.exports = [
   {
+    name: 'strapi::errors',  // ✅ Required for error handling
+  },
+  {
     name: 'strapi::security',
   },
   {
@@ -16,6 +19,9 @@ module.exports = [
   },
   {
     name: 'strapi::body',
+  },
+  {
+    name: 'strapi::session',  // ✅ Remove this if not using sessions
   },
   {
     name: 'strapi::favicon',
